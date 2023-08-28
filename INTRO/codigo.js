3344f36fa61edf512dc2aa31cb56fa8c8cae6cef
renzofranchetto <renzofrancheto2003@gmail.com>
2023-08-27 21:50:07 -0300

    f

---
+++ INTRO/codigo.js
@@ -0,0 +1,43 @@
+// // // let cofla = prompt("ingresa monto");
+// // // let unKilo = 2;
+
+// // // if (cofla < 1){
+// // //     document.write("no te alcanza para helado")
+// // // }
+// // // else if (cofla >=1 && cofla<=2){
+// // //  document.write("te alcanza para helado de agua")
+// // // }
+// // // else if(cofla >=2 ){
+// // //     document.write("y...yo que vos me llevo 1kg");
+// // //     vuelto = cofla - unKilo;
+// // //     document.write(` tu vuelto es ${vuelto}`)
+// // // }
+// // //array
+// // let moto = ["tesla","yamaha","bmw"]
+
+
+// // //array asociativo
+// // let auto = {
+// //     nombre:"ram",
+// //     km:"10"
+// // };
+
+// // let nombre = auto["nombre"];
+// // let km = auto["km"];
+
+// // marcaAndKm = `mi auto es marca: ${nombre}<br> y tiene:${km}km`;
+
+// // document.write(marcaAndKm)
+
+// // //for 
+// // for (let i=0;i<=6;i++){
+    
+// //     document.write(`<br> ${i}`)
+// // }
+
+// // let animales = ["jaguar","leon"];
+// // for (animal of animales) {
+// //     document.write(`<br>${animal}`)
+// // }
+console.log("hola")
+console.log("hola")
